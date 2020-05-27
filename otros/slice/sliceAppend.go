f21a73f13ad5854d4ab3b189dc0f52f78ba29e15
NicoRB28 <nicolasrodriguezboschi@gmail.com>
2020-05-26 21:47:01 -0300

    solucion error

--- otros/slice/sliceAppend.go
+++ otros/slice/sliceAppend.go
@@ -7,8 +7,11 @@ import (
 func main() {
 
 	slice := []int{}
-
+	slice2 := []int{9, 10, 11, 12}
 	slice = append(slice, 1, 2, 3, 4, 5, 6, 7, 8)
 
+	fmt.Println(slice)
+
+	slice = append(slice, slice2...)
 	fmt.Println(slice)
 }
